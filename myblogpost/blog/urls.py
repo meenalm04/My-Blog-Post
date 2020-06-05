edf504091fd49269e7bb81c54c9c405dc892286b
AdityaTeng <adityateng@gmail.com>
2020-06-05 23:18:03 +0530

    https://github.com/meenalm04/My-Blog-Post/issues/4 resolved

--- myblogpost/blog/urls.py
+++ myblogpost/blog/urls.py
@@ -6,12 +6,4 @@ urlpatterns = [
     path('about/', views.about, name='blog-about'),
     path('', views.home, name='blog-home'),
     path('post_detail/<int:id>', views.DetailView, name='blog-Detail-View' )
-    # path('post_detail/',views.post_detail, name='blog-post_detail')
-    # path('', views.PostDetail.as_view(), name='post_detail'),
-    # for looping through posts
-    # path('', views.PostList.as_view(), name='home'), 
-    
-    # path('object.title/', views.PostDetail.as_view(), name='post_detail'),
-   
-    
 ]
